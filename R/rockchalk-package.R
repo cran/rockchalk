82782a81b7cbd24a4d42e375effce1af668ea248
Paul E. Johnson <pauljohn@ku.edu>
2011-11-02 00:00:00 +0000

    version 1.0

---
+++ R/rockchalk-package.R
@@ -0,0 +1,21 @@
+#' Miscellaneous regression functions
+#' 
+#'  
+#' \tabular{ll}{ Package: \tab rockchalk\cr Type: \tab Package\cr Version: \tab
+#' 1.0\cr Date: \tab 2011-08-29\cr License: \tab GPL >= 3\cr LazyLoad: \tab
+#' yes\cr } ~~ An overview of how to use the package, including the most
+#' important ~~ ~~ functions ~~
+#' 
+#' @name rockchalk-package
+#' @aliases rockchalk-package rockchalk
+#' @docType package
+#' @author Paul Johnson <pauljohn@@ku.edu>
+#' 
+#' Maintainer: Paul Johnson <pauljohn@@ku.edu>
+#' @references http://pj.freefaculty.org/R
+#' @keywords regression hplot
+
+NULL
+
+
+
